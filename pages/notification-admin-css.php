442f56f42c1c54d8754642185b2a601ae00e7d1d
Benaceur <Benaceur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-10 12:11:53 +0000

    derniere mod
    git-svn-id: https://plugins.svn.wordpress.org/notification-admin-panel-benaceur/trunk@1086329 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pages/notification-admin-css.php
+++ pages/notification-admin-css.php
@@ -1,4 +1,4 @@
-<style>
+<style type="text/css">
 <?php if ( !get_option( 'notification_admin_benaceur_disable_this_font' ) )  { ?>
 @font-face {
     font-family:"DroidKufi_Ben";
@@ -15,5 +15,34 @@
     border-radius:<?php echo get_option( 'notification_admin_benaceur_border_radius' ) ; ?>px;
     -moz-border-radius:<?php echo get_option( 'notification_admin_benaceur_border_radius' ) ; ?>px;
     -webkit-border-radius:<?php echo get_option( 'notification_admin_benaceur_border_radius' ) ; ?>px;
+}
+.important-nab_a2 { 
+    background-color:<?php if (!get_option( 'notification_admin_benaceur_color_back2' )) echo get_option( 'notification_admin_benaceur_color_back2' ) ; else  echo '#f5d6d6'; ?>;
+    border-top:<?php if (get_option( 'notification_admin_benaceur_border_top' )) echo get_option( 'notification_admin_benaceur_border_top'); elseif (get_option( 'notification_admin_benaceur_border_top') == '0')  echo "0"; else echo "1"; ?>px solid <?php if (!get_option( 'notification_admin_benaceur_color_border' )) echo get_option( 'notification_admin_benaceur_color_border' ) ; else  echo '#CE1031'; ?>;
+    border-bottom:<?php if (get_option( 'notification_admin_benaceur_border_bottom' )) echo get_option( 'notification_admin_benaceur_border_bottom'); elseif (get_option( 'notification_admin_benaceur_border_bottom') == '0')  echo "0"; else echo "1"; ?>px solid <?php if (!get_option( 'notification_admin_benaceur_color_border' )) echo get_option( 'notification_admin_benaceur_color_border' ) ; else  echo '#CE1031'; ?>;
+    border-right:<?php if (get_option( 'notification_admin_benaceur_border_right' )) echo get_option( 'notification_admin_benaceur_border_right'); elseif (get_option( 'notification_admin_benaceur_border_right') == '0')  echo "0"; else echo "1"; ?>px solid <?php if (!get_option( 'notification_admin_benaceur_color_border' )) echo get_option( 'notification_admin_benaceur_color_border' ) ; else  echo '#CE1031'; ?>;
+    border-left:<?php if (get_option( 'notification_admin_benaceur_border_left' )) echo get_option( 'notification_admin_benaceur_border_left'); elseif (get_option( 'notification_admin_benaceur_border_left') == '0')  echo "0"; else echo "1"; ?>px solid <?php if (!get_option( 'notification_admin_benaceur_color_border' )) echo get_option( 'notification_admin_benaceur_color_border' ) ; else  echo '#CE1031'; ?>;
+	box-shadow:<?php echo get_option('notification_admin_benaceur_box_shadow'); ?> <?php if (get_option( 'notification_admin_benaceur_box_shadow_v' )) echo get_option( 'notification_admin_benaceur_box_shadow_v'); elseif (get_option( 'notification_admin_benaceur_box_shadow_v') == '0')  echo "0"; else echo "4"; ?>px <?php if (!get_option( 'notification_admin_benaceur_box_shadow_color' )) echo get_option( 'notification_admin_benaceur_box_shadow_color' ) ; else  echo '#888888'; ?>;
+	line-height:2;
+    border-radius:<?php echo get_option( 'notification_admin_benaceur_border_radius' ) ; ?>px;
+    width:<?php if (!get_option( 'notification_admin_benaceur_width' )) echo get_option( 'notification_admin_benaceur_width' ) ; else  echo '100%'; ?>;
+    -moz-border-radius:<?php echo get_option( 'notification_admin_benaceur_border_radius' ) ; ?>px;
+    -webkit-border-radius:<?php echo get_option( 'notification_admin_benaceur_border_radius' ) ; ?>px;
+	margin-top:<?php if (get_option( 'notification_admin_benaceur_margin_top' )) echo get_option( 'notification_admin_benaceur_margin_top'); else echo "0"; ?>px;
+	margin-bottom:<?php if (get_option( 'notification_admin_benaceur_margin_bottom' )) echo get_option( 'notification_admin_benaceur_margin_bottom'); else echo "0"; ?>px;
+    opacity:<?php if (get_option( 'notification_admin_benaceur_opacity' )) echo get_option( 'notification_admin_benaceur_opacity'); elseif (get_option( 'notification_admin_benaceur_opacity') == '0')  echo "0"; else echo "1"; ?>; 
+ }
+.important-nab_a3 {
+    font-family:<?php if (!get_option( 'notification_admin_benaceur_font' )) echo get_option( 'notification_admin_benaceur_font' ) ; else  echo 'DroidKufi_Ben, Arial'; ?>;
+    font-size:<?php if (!get_option( 'notification_admin_benaceur_font_size' )) echo get_option( 'notification_admin_benaceur_font_size' ) ; else  echo '14'; ?>px;
+    color:<?php if (!get_option( 'notification_admin_benaceur_color_text' )) echo get_option( 'notification_admin_benaceur_color_text' ) ; else  echo '#000000'; ?>; 
+    font-weight:<?php echo get_option('notification_admin_benaceur_font_weight') ;?>;
+    text-align:<?php if (!get_option( 'notification_admin_benaceur_text_align' )) echo get_option( 'notification_admin_benaceur_text_align' ) ; else  echo 'center'; ?>;
+	text-shadow:<?php echo get_option('notification_admin_benaceur_text_shadow'); ?> <?php if (!get_option( 'notification_admin_benaceur_text_shadow_color' )) echo get_option( 'notification_admin_benaceur_text_shadow_color' ) ; else  echo '#000000'; ?>;
+	padding-top:<?php if (get_option( 'notification_admin_benaceur_padding_top' )) echo get_option( 'notification_admin_benaceur_padding_top'); elseif (get_option( 'notification_admin_benaceur_padding_top') == '0')  echo "0"; else echo "1"; ?>px;
+	padding-bottom:<?php if (get_option( 'notification_admin_benaceur_padding_bottom' )) echo get_option( 'notification_admin_benaceur_padding_bottom'); elseif (get_option( 'notification_admin_benaceur_padding_bottom') == '0')  echo "0"; else echo "1"; ?>px;
+	padding-right:6px;
+	padding-left:6px;
+	visibility: visible; 
 }
 </style>
