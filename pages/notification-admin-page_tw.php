20cba405569a2cd42647b476f4819683cc27af34
Benaceur <Benaceur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-10 10:39:03 +0000

    First commit
    git-svn-id: https://plugins.svn.wordpress.org/notification-admin-panel-benaceur/trunk@1086271 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pages/notification-admin-page_tw.php
@@ -0,0 +1,29 @@
+<?php  
+?>		
+
+<div id="my_wellcome_panel">
+<?php if ( get_option('notification_admin_benaceur_align_msg') == 'center' ) { ?>
+<div align="center">
+<?php } ?>
+<?php if ( get_option('notification_admin_benaceur_align_msg') == 'right' ) { ?>
+<div align="right">
+<?php } ?>
+<?php if ( get_option('notification_admin_benaceur_align_msg') == 'left' ) { ?>
+<div align="left">
+<?php } ?>
+
+<div class="important-nab_a2" id="my_wellcome_panel"><div
+<?php if ( !get_option('notification_admin_benaceur_disable_clignotement')) { ?>
+ id="my_wellcome_panel-b-note-nab_a" 
+<?php } ?>
+ class="important-nab_a">
+<div class="important-nab_a3">
+<?php  echo get_option('notification_admin_benaceur_text_tw');  ?>
+</div></div></div>
+
+</div>
+</div>
+
+<?php include('notification-admin.php'); ?>
+
+
