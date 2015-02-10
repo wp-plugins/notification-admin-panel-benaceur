1adc042979fd90b6c3c29356b9ee8d471cadd1d0
Benaceur <Benaceur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-10 11:55:32 +0000

    admin nb
    git-svn-id: https://plugins.svn.wordpress.org/notification-admin-panel-benaceur/trunk@1086310 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pages/notification-admin-css.php
@@ -0,0 +1,12 @@
+<style>
+<?php if ( !get_option( 'notification_admin_benaceur_disable_this_font' ) )  { ?>
+@font-face {
+    font-family:"DroidKufi_Ben";
+    <?php echo 'src: url(' . plugins_url( '../font/droidkufi-regular.eot', __FILE__ ) . '); '; ?>
+}
+@font-face {
+    font-family:"DroidKufi_Ben";
+    <?php echo 'src: url(' . plugins_url( '../font/droidkufi-regular.ttf', __FILE__ ) . '); '; ?>
+}
+<?php } ?>
+</style>
